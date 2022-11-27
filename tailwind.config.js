d1225b275865c1756d60096a0001e862370749c9
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-27 18:52:40 +0500

    build: advanced tailwind + eslint configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
+        './pages/**/*.{html,js,ts,jsx,tsx}',
+        './components/**/*.{html,js,ts,jsx,tsx}',
+        './sections/**/*.{html,js,ts,jsx,tsx}',
+        './styles/**/*.{html,js,ts,jsx,tsx}',
     ],
+    mode: 'jit',
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'primary-black': '#1A232E',
+                'secondary-white': '#c7c7c7',
+            },
+            transitionTimingFunction: {
+                'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+            },
+        },
     },
     plugins: [],
 }
