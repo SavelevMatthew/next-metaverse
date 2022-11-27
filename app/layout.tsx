200b676343a89b04f95333d8c5a9fa85b9723c20
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-28 01:08:29 +0500

    fix: replaced next/head with default head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import '../styles/globals.css'
 
 type BaseLayoutProps = {
@@ -9,11 +8,11 @@ type BaseLayoutProps = {
 const BaseLayout: React.FC<BaseLayoutProps> = ({ children }) => {
     return (
         <html lang='en'>
-            <Head>
+            <head>
                 <title>MetaVersus</title>
                 <link rel='preconnect' href='https://stijndv.com' />
                 <link rel='stylesheet' href='https://stijndv.com/fonts/Eudoxus-Sans.css' />
-            </Head>
+            </head>
             <body>{children}</body>
         </html>
     )
