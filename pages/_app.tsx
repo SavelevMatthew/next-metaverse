725981bb03a347912e9749b25cffd046f3ac1647
Matthew Savelev <savelevmatthew@gmail.com>
2022-11-27 19:07:19 +0500

    build: added default boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+    return (
+        <>
+            <Head>
+                <title>MetaVersus</title>
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <link rel='icon' href='/favicon.ico' />
+                <link rel='preconnect' href='https://stijndv.com' />
+                <link rel='stylesheet' href='https://stijndv.com/fonts/Eudoxus-Sans.css' />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
+
+export default App
